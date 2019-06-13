401d94719a568c85e6733dbcc797598122e68e6d
Nicholas Armstrong <nick@nicks-mail.com>
2019-06-13 21:45:41 +1000

    Router, Bootstrap, Navbar.

--- src/main/static/app/app.module.ts
+++ src/main/static/app/app.module.ts
@@ -3,13 +3,21 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        MainNavComponent,
+        TestComponent
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         AppRoutingModule
     ],
     providers: [],
